d2ff5ef788aa85c571124553d837de3fa49d1656
Dunaas <39232355+Dunaas@users.noreply.github.com>
2018-08-20 02:09:57 -0300

    Update scrap.js

--- scrap.js
+++ scrap.js
@@ -6,7 +6,7 @@ var rShow = 4;
 
 setInterval(function() {
 request('http://www.brawlhalla.com/community/', function (error, response, html) {
-  if (!error && response.statusCode == process.env.PORT || 200) {
+  if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     $('h2.entry-title').each(function(i, element){
       var a = $(this);
@@ -15,7 +15,7 @@ request('http://www.brawlhalla.com/community/', function (error, response, html)
         rNum++;
         console.log('CONTEM!');
         request('http://www.brawlhalla.com/news/community-roundup-42/', function (error, response, html) {
-        if (!error && response.statusCode == process.env.PORT || 200) {
+        if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         $('div.et_post_meta_wrapper').each(function(i, element){
         var cc = $(this).prev().text().match(/.....-.....-...../g);
@@ -33,7 +33,7 @@ request('http://www.brawlhalla.com/community/', function (error, response, html)
 
 setInterval(function() {
   request('http://www.brawlhalla.com/community/', function (error, response, html) {
-    if (!error && response.statusCode == process.env.PORT || 200) {
+    if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
       $('h2.entry-title').each(function(i, element){
         var a = $(this);
@@ -42,7 +42,7 @@ setInterval(function() {
           console.log('CONTEM SHOW!');
           rShow++;
           request('http://www.brawlhalla.com/news/brawlhalla-community-art-showcase-4/', function (error, response, html) {
-          if (!error && response.statusCode == process.env.PORT || 200) {
+          if (!error && response.statusCode == 200) {
           var $ = cheerio.load(html);
           $('div.et_post_meta_wrapper').each(function(i, element){
           var cc = $(this).prev().text().match(/.....-.....-...../g);
