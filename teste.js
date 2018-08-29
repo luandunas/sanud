dea66ef00d4e7f304b820893afc40c9df8516816
Dunaas <39232355+Dunaas@users.noreply.github.com>
2018-08-29 04:00:12 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+let config = JSON.parse(fs.readFileSync('./teste.json'));
+bot.on("messageCreate", (msg) => {
+    if (msg.author.bot) return;
+    let command = msg.content.split(" ")[0];
+    let args = msg.content.split(" ").slice(1);
+    if (command === "!t") {
+        fs.writeFile("teste.json", 'ok', function(err) {});
+        bot.createMessage(msg.channel.id, "done!");
+    }
+});
