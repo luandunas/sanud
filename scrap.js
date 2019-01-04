42985712790b39b47ec5b64a280cc2b295f173cc
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-01-04 03:38:54 -0200

    Update scrap.js

--- scrap.js
+++ scrap.js
@@ -4,7 +4,6 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var myJsonAPI = require('myjson-api');
 var json;
-var mandarUm = false;
 bot.on("ready", () => {
     console.log("Ready!");
 });
@@ -14,83 +13,27 @@ request({
     json = JSON.parse(body);
 });
 setInterval(function() {
-    request({
-    url: "https://api.myjson.com/bins/njwy4",
-    }, function(err, res, body) {
-    json = JSON.parse(body);
-    });
-    request('http://www.brawlhalla.com/community/', function(error, response, html) {
-        if (!error && response.statusCode == 200) {
+    request('http://www.brawlhalla.com/news/brawlhalla-community-art-showcase-' + json.showcase + '/', function(error, response, html) {
+        if (!error && response.statusCode != 404 && response.statusCode == 200) {
+            console.log('CONTEM ROUNDUP!');
             var $ = cheerio.load(html);
-            $('h2.entry-title').each(function(i, element) {
-                var a = $(this);
-                var sites = a.text();
-                if (sites.indexOf('Community Roundup #' + json.roundup) != -1) {
-                    console.log('CONTEM ROUNDUP!');
-                    request('http://www.brawlhalla.com/news/community-roundup-' + json.roundup + '/', function(error, response, html) {
-                        if (!error && response.statusCode == 200) {
-                            var $ = cheerio.load(html);
-                            $('div.et_post_meta_wrapper').each(function(i, element) {
-                                var cc = $(this).prev().text().match(/.....-.....-...../g);
-                                if (cc != null) {
-                                    json.roundup = json.roundup += 1;
-                                    myJsonAPI.update("njwy4", json).then((updatedJSON) => console.log(updatedJSON));
-                                    request({
-                                        url: "https://api.myjson.com/bins/njwy4",
-                                    }, function(err, res, body) {
-                                        json = JSON.parse(body);
-                                    });
-                                    if (mandarUm == false) {
-                                        bot.createMessage('483152613458837515', 'CCS: <@211962239433834498> <@214946188108103680> \n', 'CCS: ' + cc.toString().replace(/,/g, '\n'));
-                                        bot.createMessage('516083352290197504', 'CCS: <@211962239433834498> <@214946188108103680> \n', 'CCS: ' + cc.toString().replace(/,/g, '\n'));
-                                        setTimeout(function() {
-                                            mandarUm = false;
-                                        }, 5000);
-                                        mandarUm = true;
-                                    }
-                                }
-                            });
-                        }
+            $('div.et_post_meta_wrapper').each(function(i, element) {
+                var cc = $(this).prev().text().match(/.....-.....-...../g);
+                if (cc != null) {
+                    json.showcase = json.showcase += 1;
+                    myJsonAPI.update("njwy4", json).then((updatedJSON) => console.log(updatedJSON));
+                    request({
+                        url: "https://api.myjson.com/bins/njwy4",
+                    }, function(err, res, body) {
+                        json = JSON.parse(body);
                     });
-                }
-            });
-        }
-    });
-}, 5000);
-setInterval(function() {
-    request('http://www.brawlhalla.com/community/', function(error, response, html) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(html);
-            $('h2.entry-title').each(function(i, element) {
-                var a = $(this);
-                var sites = a.text();
-                if (sites.indexOf('Brawlhalla Community Art Showcase #' + json.showcase) != -1) {
-                    console.log('CONTEM SHOW!');
-                    request('http://www.brawlhalla.com/news/brawlhalla-community-art-showcase-' + json.showcase + '/', function(error, response, html) {
-                        if (!error && response.statusCode == 200) {
-                            var $ = cheerio.load(html);
-                            $('div.et_post_meta_wrapper').each(function(i, element) {
-                                var cc = $(this).prev().text().match(/.....-.....-...../g);
-                                if (cc != null) {
-                                    json.showcase = json.showcase += 1;
-                                    myJsonAPI.update("njwy4", json).then((updatedJSON) => console.log(updatedJSON));
-                                    request({
-                                        url: "https://api.myjson.com/bins/njwy4",
-                                    }, function(err, res, body) {
-                                        json = JSON.parse(body);
-                                    });
-                                    if (mandarUm == false) {
-                                        bot.createMessage('483152613458837515', 'CCS: <@211962239433834498> <@214946188108103680> \n' + cc.toString().replace(/,/g, '\n'));
-                                        bot.createMessage('516083352290197504', 'CCS: <@211962239433834498> <@214946188108103680> \n' + cc.toString().replace(/,/g, '\n'));
-                                        setTimeout(function() {
-                                            mandarUm = false;
-                                        }, 5000);
-                                        mandarUm = true;
-                                    }
-                                }
-                            });
-                        }
-                    });
+                    if (mandarUm == false) {
+                        bot.createMessage('516083352290197504', `**CCS: <@&481690111675727882>  <@&519259085053493249> \n**${cc.toString().replace(/,/g, '\n')}`);
+                        setTimeout(function() {
+                            mandarUm = false;
+                        }, 5000);
+                        mandarUm = true;
+                    }
                 }
             });
         }
