d169569b4246f8f20695528696d35d4cf32ac54f
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-05-27 10:10:46 -0300

    Update neonImg.js

--- functions/puppeteer/neonImg.js
+++ functions/puppeteer/neonImg.js
@@ -1,5 +1,5 @@
 const neonImg = async(msg, args) => {
-    const browser = await puppeteer.launch({'args':['--no-sandbox','--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({'args':['--no-sandbox','--disable-setuid-sandbox']})
 	const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
     await page.goto('https://photofunia.com/categories/all_effects/neon-sign', {
