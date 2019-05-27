78a85841939a2240f1ca6b148448a149be35af9a
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-05-27 09:41:46 -0300

    Create voiceJoin.js

---
+++ functions/voiceEvents/voiceJoin.js
@@ -0,0 +1,13 @@
+bot.on("voiceChannelJoin", (member, newChannel) => {
+  cid = member.voiceState.channelID;
+  if (power == true) {
+    newChannel.voiceMembers.forEach(function(m) {
+      if (m.roles.indexOf('576512821056110621') == -1) {
+        m.edit({
+          mute: true
+        })
+      }
+    })
+  }
+  if (newChannel.id == member.voiceState.channelID && member) {}
+});
