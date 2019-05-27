f8488867b52525646c001eab729f6afa3f03c71e
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-05-27 09:43:03 -0300

    Create topPorn.js

---
+++ functions/scrap/topPorn.js
@@ -0,0 +1,35 @@
+function topPorn(channel){
+    request('https://www.xvideos.com/pornstars-index', function(error, response, html) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        $('div.mozaique').each(function(i, element) {
+          var a = $(this);
+          var sites = a.children('div').find('.profile-name').text();
+          $(a.children('div').find('p.profile-name').find('a').each(function(i, elem) {
+            if (rank != 10) {
+              mesg += '#' + rank + ' ' + $(elem).text() + '\n';
+              rank++
+            }
+          }))
+          bot.createMessage(channel, {
+            embed: {
+              author: {
+                name: "Sanud"
+              },
+              color: 0x00FF88,
+              fields: [{
+                name: "Top 10",
+                value: mesg
+              }],
+              footer: { // Footer text
+                text: 'XVideos!'
+              }
+            }
+          });
+          mesg = "";
+          rank = "";
+          //console.log(sites);
+        })
+      }
+    })
+};
