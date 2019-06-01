6d7a27bf749a8fdb87eadfc07faa28bedb45e5d0
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-06-01 09:18:48 -0300

    Update topPorn.js

--- functions/scrap/topPorn.js
+++ functions/scrap/topPorn.js
@@ -6,7 +6,7 @@ function topPorn(channel){
           var a = $(this);
           var sites = a.children('div').find('.profile-name').text();
           $(a.children('div').find('p.profile-name').find('a').each(function(i, elem) {
-            if (rank != 10) {
+            if (rank <= 10) {
                 rank = 0;
               mesg += '#' + rank + ' ' + $(elem).text() + '\n';
               rank++
@@ -28,7 +28,7 @@ function topPorn(channel){
             }
           });
           mesg = "";
-          rank = "";
+          rank = 0;
           //console.log(sites);
         })
       }
