0c4a1d442b2f7b74807656f8737792e143f2e10c
Dunaas <39232355+Dunaas@users.noreply.github.com>
2019-07-12 15:04:47 -0300

    Update ccscrap.js

--- functions/scrap/ccscrap.js
+++ functions/scrap/ccscrap.js
@@ -1,18 +1,17 @@
 FB.options({accessToken: process.env.FB_TOKEN, appSecretProof: process.env.FB_ID});
 console.log(fboptions);
-
 function requestjson() {
     setTimeout(function() {
         request({
             url: 'https://api.myjson.com/bins/njwy4',
         }, function(err, res, body) {
             json = JSON.parse(body);
-            console.log(body);
+            console.log(json);
         });
     }, 2000);
 }
 
-function fbsend() {
+function fbsend(cc) {
     if (cc != undefined) {
         FB.api('yggdrasilbrz/feed', 'post', {
             message: `Community Colors! \n ${cc} \n Nos siga para ser notificado quando houver nova showcase! Entre em nosso discord para receber notificação por lá https://discord.gg/VxVujFe`
@@ -40,8 +39,8 @@ setInterval(function() {
                     request(elem.attribs['href'], function(error, response, body) {
                         if (!error && response.statusCode != 404 && response.statusCode == 200) {
                             cc = cheerio.load(body);
-                            bot.createMessage('564337011268517890', `**CCS: <@&481690111675727882>  <@211962239433834498> \n ${cc.text().match(/......-....../g).toString().replace(/,/g, '\n')}**`);
-                            fbsend();
+                            bot.createMessage('564337011268517890', `**CCS: <@211962239433834498> \n ${cc.text().match(/[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]/g).toString().replace(/,/g, '\n')}**`);
+                            fbsend(cc.text().match(/[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]-[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]/g).toString().replace(/,/g, '\n'));
                         }
                         json = JSON.stringify(json).replace('}}', `},"${elem.attribs['href']}":{"link":"${elem.attribs['href']}"}}`);
                         json = JSON.parse(json);
